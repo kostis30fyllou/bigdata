2975a4ad98ab9258c9f6a43e24baf737fde6ac87
kostis30fyllou <cs2180018@di.uoa.gr>
2019-02-03 22:33:07 +0200

    Test for clf

--- example.py
+++ example.py
@@ -5,12 +5,13 @@ from utils import *
 from sklearn import svm
 import numpy as np
 from utils import *
-from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer,TfidfTransformer
+from gensim.sklearn_api.ldamodel import LdaTransformer
  
 from sklearn.pipeline import Pipeline
 
-documents = read_csv("train_set.csv").head(100).Content
-documents2 = read_csv("train_set.csv").head(100).Category
+documents = read_csv("train_set.csv").head(1000).Content
+categories = read_csv("train_set.csv").head(1000).Category
 test = read_csv("test_set.csv").iloc[0].Content
 #test2 = read_csv("test_set.csv").iloc[1].Content
 
@@ -19,16 +20,18 @@ texts = [[text for text in doc.split()] for doc in documents]
 dictionary = corpora.Dictionary(texts)
 
 mycorpus = [dictionary.doc2bow(doc, allow_update=True) for doc in texts]
-
+test = dictionary.doc2bow([test])
+model = LdaTransformer(id2word=dictionary, num_topics=5, iterations=20)
+model.fit(mycorpus)
+X = model.transform(mycorpus)
 #vectorizer = TfidfVectorizer()
 #vectorizer.fit(documents)
 #X = vectorizer.transform(documents)
 #y = documents2
-#s = vectorizer.transform([test])
+s = model.transform([test])
 #s2 = vectorizer.transform([test2])
 clf = svm.SVC(gamma='scale')
-pipeline = Pipeline([('bow', mycorpus), ('classifier', clf)])
-pipeline.fit(documents, documents2)
-print(pipeline.score(documents, documents2))
+clf.fit(X, categories)
+print(clf.predict(s))
 #print(clf.predict(s2))
 
